61c5794d91993a6969309fa5b9d95b1a9c178d93
Ahmad Hamza <priahmz@gmail.com>
2026-02-05 00:07:53 +0100

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,6 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAI1MrcP977UgRLQoePxl64JOSyj9v4WpI",
@@ -8,10 +9,12 @@ const firebaseConfig = {
   projectId: "xtaagc",
   storageBucket: "xtaagc.firebasestorage.app",
   messagingSenderId: "256073982437",
-  appId: "1:256073982437:web:8da63bb7acc86c0ca98f0c"
+  appId: "1:256073982437:web:8da63bb7acc86c0ca98f0c",
+  measurementId: "G-1D2RPBMML3"
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
