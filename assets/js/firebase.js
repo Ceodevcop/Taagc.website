64fd7f62a2fbde2865369e5aff10a06aab6a3d37
Ahmad Hamza <priahmz@gmail.com>
2026-02-05 02:07:00 +0100

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAI1MrcP977UgRLQoePxl64JOSyj9v4WpI",
@@ -14,7 +13,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
