83a4ef7935af0f1c407e7981346a8bcf94bee103
Ahmad Hamza <priahmz@gmail.com>
2026-02-05 02:07:47 +0100

    Update jsonLoader.js

--- assets/js/jsonLoader.js
+++ assets/js/jsonLoader.js
@@ -1,7 +1,10 @@
 export async function loadJSON(path) {
-  const res = await fetch(path);
-  if (!res.ok) {
-    throw new Error("Failed to load " + path);
+  try {
+    const res = await fetch(path);
+    if (!res.ok) throw new Error(`Failed to load ${path}`);
+    return await res.json();
+  } catch (err) {
+    console.error(err);
+    return null;
   }
-  return await res.json();
 }
