7bac86f4bd3530d83cab79873ad08e70b414ea23
Ahmad Hamza <priahmz@gmail.com>
2026-02-05 01:23:33 +0100

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -3,28 +3,27 @@ import { signInWithEmailAndPassword } from
   "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { doc, getDoc } from
   "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { loadJSON } from "./jsonLoader.js";
 
 window.loginUser = async function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  const role = document.getElementById("role").value;
+  const selectedRole = document.getElementById("role").value;
 
-  try {
-    const cred = await signInWithEmailAndPassword(auth, email, password);
-    const uid = cred.user.uid;
+  const roles = await loadJSON("/assets/json/roles.json");
 
-    const snap = await getDoc(doc(db, "users", uid));
-    if (!snap.exists()) return alert("User role not assigned");
+  const cred = await signInWithEmailAndPassword(auth, email, password);
+  const uid = cred.user.uid;
 
-    const userRole = snap.data().role;
+  const snap = await getDoc(doc(db, "users", uid));
+  if (!snap.exists()) return alert("User role not assigned");
 
-    if (userRole !== role) {
-      alert("Role mismatch!");
-      return;
-    }
+  const role = snap.data().role;
 
-    window.location.href = `dashboard/${role}.html`;
-  } catch (err) {
-    alert(err.message);
+  if (role !== selectedRole) {
+    alert("Role mismatch");
+    return;
   }
+
+  window.location.href = roles[role].dashboard;
 };
