01758711c961e218d2a18aecec5f7133e9e1986d
Ahmad Hamza <priahmz@gmail.com>
2026-02-06 12:20:55 +0100

    Update audit.js

--- assets/js/audit.js
+++ assets/js/audit.js
@@ -1,15 +1,19 @@
-import { db } from "./firebase.js";
-import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { auth, db } from "./firebase.js";
+import { signInWithEmailAndPassword } from
+"https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { doc, getDoc } from
+"https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-export async function logAction(uid, action) {
-  try {
-    await addDoc(collection(db, "auditLogs"), {
-      uid,
-      action,
-      timestamp: serverTimestamp()
-    });
-    console.log("Audit logged:", action);
-  } catch (err) {
-    console.error("Failed to log audit", err);
-  }
-}
+window.login = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const cred = await signInWithEmailAndPassword(auth, email, password);
+  const snap = await getDoc(doc(db, "users", cred.user.uid));
+
+  const role = snap.data().role;
+
+  if (role === "admin") location.href = "dashboard/admin.html";
+  if (role === "investor") location.href = "dashboard/investor.html";
+  if (role === "client") location.href = "dashboard/client.html";
+};
