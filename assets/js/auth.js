1c8b41810e6171d74d073df83486cf23012fcc4d
Ahmad Hamza <priahmz@gmail.com>
2026-02-06 19:24:22 +0100

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,23 +1,40 @@
-import { auth, db } from "./firebase.js";
-import { signInWithEmailAndPassword } from
-"https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { doc, getDoc } from
-"https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import {
+  getFirestore,
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const auth = getAuth();
+const db = getFirestore();
 
 window.login = async () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   const cred = await signInWithEmailAndPassword(auth, email, password);
-  if (!cred.user.emailVerified) {
-    alert("Please verify your email first.");
+
+  const ref = doc(db, "users", cred.user.uid);
+  const snap = await getDoc(ref);
+
+  if (!snap.exists()) {
+    alert("User role not assigned");
     return;
   }
 
-  const snap = await getDoc(doc(db, "users", cred.user.uid));
   const role = snap.data().role;
 
-  if (role === "admin") window.location = "dashboard/admin.html";
-  if (role === "investor") window.location = "dashboard/investor.html";
-  if (role === "client") window.location = "dashboard/client.html";
+  if (role === "admin") {
+    window.location.href = "/dashboard/admin.html";
+  } else if (role === "client") {
+    window.location.href = "/dashboard/client.html";
+  } else if (role === "investor") {
+    window.location.href = "/dashboard/investor.html";
+  } else {
+    alert("Invalid role");
+  }
 };
