a9fd555d65745e2ad0e25f8de4d58ebaa353a6a0
Ahmad Hamza <priahmz@gmail.com>
2026-02-06 12:22:12 +0100

    Update logout.js

--- assets/js/logout.js
+++ assets/js/logout.js
@@ -1,8 +1,8 @@
 import { auth } from "./firebase.js";
 import { signOut } from
-"https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+"https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-window.logout = async function () {
+window.logout = async () => {
   await signOut(auth);
-  window.location.href = "../login.html";
+  location.href = "../login.html";
 };
