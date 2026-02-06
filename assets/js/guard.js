9f76cbb0c23c8f7f2f71d7eaa524ce349c08ecae
Ahmad Hamza <priahmz@gmail.com>
2026-02-06 12:54:14 +0100

    Update guard.js

--- assets/js/guard.js
+++ assets/js/guard.js
@@ -1,18 +1,23 @@
 import { auth, db } from "./firebase.js";
 import { onAuthStateChanged } from
-  "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+"https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { doc, getDoc } from
-  "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+"https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-export function protectPage(requiredRole) {
+export function protectPage(allowedRoles = []) {
   onAuthStateChanged(auth, async (user) => {
     if (!user) {
       window.location.href = "../login.html";
       return;
     }
-
     const snap = await getDoc(doc(db, "users", user.uid));
-    if (!snap.exists() || snap.data().role !== requiredRole) {
+    if (!snap.exists()) {
+      await auth.signOut();
+      window.location.href = "../login.html";
+      return;
+    }
+    const role = snap.data().role;
+    if (!allowedRoles.includes(role)) {
       alert("Access denied");
       window.location.href = "../login.html";
     }
