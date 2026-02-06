aab32a4e8150178baa0cbc2321d16bc47c6f2738
Ahmad Hamza <priahmz@gmail.com>
2026-02-06 12:14:20 +0100

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAI1MrcP977UgRLQoePxl64JOSyj9v4WpI",
@@ -8,10 +8,9 @@ const firebaseConfig = {
   projectId: "xtaagc",
   storageBucket: "xtaagc.firebasestorage.app",
   messagingSenderId: "256073982437",
-  appId: "1:256073982437:web:8da63bb7acc86c0ca98f0c",
-  measurementId: "G-1D2RPBMML3"
+  appId: "1:256073982437:web:8da63bb7acc86c0ca98f0c"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
