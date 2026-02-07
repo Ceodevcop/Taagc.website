935dd0433ec225c0aab62af907593d294766e2e0
Ahmad Hamza <priahmz@gmail.com>
2026-02-07 02:00:19 +0100

    Delete assets/js directory

--- assets/js/logout.js
+++
@@ -1,8 +0,0 @@
-import { auth } from "./firebase.js";
-import { signOut } from
-"https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-
-window.logout = async () => {
-  await signOut(auth);
-  window.location.href = "../login.html";
-};
