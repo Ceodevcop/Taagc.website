94fe8853d12a54048a9dfbd47094a806d1433727
Ahmad Hamza <priahmz@gmail.com>
2026-02-09 00:01:23 +0100

    Create guard.js

---
+++ js/guard.js
@@ -0,0 +1,12 @@
+import { auth, db } from "./firebase.js";
+import { onAuthStateChanged } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { doc, getDoc } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+onAuthStateChanged(auth, async user => {
+  if (!user) location.href = "/login.html";
+
+  const snap = await getDoc(doc(db, "users", user.uid));
+  if (!snap.exists()) alert("User role not assigned");
+});
